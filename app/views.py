07567f2c44a359fb6234bd9a67deacc6bb416109
cartacode <cartacode@outlook.com>
2020-01-20 16:54:55 -0500

    add event page and logger

--- app/views.py
+++ app/views.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import time
 import json
 import requests
+from datetime import datetime
+from dateutil import parser
 import logging
 logger = logging.getLogger(__name__)
 
@@ -11,10 +14,28 @@ from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.urls import reverse
 from core.models import BaseUser
 from app.models import UserGroup
 
-MAILGUN_API = "https://api.mailgun.net/v3"
+MAILGUN_API_URL = "https://api.mailgun.net/v3"
+
+def mailgun_api(uri, params=None):
+    if params:
+        res = requests.get(
+                "{}/{}".format(MAILGUN_API_URL, uri),
+                auth=("api", settings.MAINGUN_KEY),
+                params=params)
+    else:
+        res = requests.get(
+                "{}/{}".format(MAILGUN_API_URL, uri),
+                auth=("api", settings.MAINGUN_KEY))
+
+    return json.loads(res.text)
+
+def get_second_key(item):
+    return item[1]
+
 
 # Create your views here.
 @login_required
@@ -30,22 +51,20 @@ def dashboard(request):
         context["allowed_list"] = allowed_list
 
     try:
-        res = requests.get("{}/domains".format(MAILGUN_API),
-                            auth=("api", settings.MAINGUN_KEY))
-        response_body = json.loads(res.text)
-        if hasattr(response_body, "items"):
-            domains = response_body["items"]
+        res = mailgun_api("domains")
+        if hasattr(res, "items"):
+            domains = res["items"]
     except Exception as e:
         context['error'] = str(e)
-        print("@@@@ eeror @@@: ", str(e))
+        logger.info("line 59: {}".format(e))
         return render(request, "app/dashboard.html", context)
 
-    print(len(domains))
-    if allowed_list:
+    if "allowed_list" in context:
         allowed_domains = list(filter(lambda d: d["name"] in context, domains))
     else:
         allowed_domains = domains
     context["allowed_domains"] = domains
+
     return render(request, "app/dashboard.html", context)
 
 @csrf_exempt
@@ -74,39 +93,72 @@ def upload_domains_to_user(request, user_id):
 def domain_stats(request, domain):
     context = {}
     try:
-        res = requests.get(
-            "{}/{}/stats/total".format(MAILGUN_API, domain),
-            auth=("api", settings.MAINGUN_KEY),
-            params={"event": ["accepted", "delivered", "failed"],
-                    "duration": "1h"})
-        response_body = json.loads(res.text)
-        print(response_body)
-        if "stats" in response_body:
-            context['stats'] = json.dumps(response_body)
+        res = mailgun_api("{}stats/total".format(domain), 
+                        {"event": ["accepted"],
+                        "duration": "1h"})
+        if "stats" in res:
+            context['stats'] = json.dumps(res)
         else:
             context['error'] = "no response"
     except Exception as e:
         context['error'] = str(e)
-    print(context)
+        logger.info("line 104: {}".format(e))
+
     return render(request, 'app/stats.html', context)
 
 
-def domain_stats(request, domain):
+def domain_events(request, domain, begin=None, end=None):
     context = {}
+    recipients = []
+    new_events = {}
     try:
-        res = requests.get(
-            "{}/{}/events".format(MAILGUN_API, domain),
-            auth=("api", settings.MAINGUN_KEY))
-        response_body = json.loads(res.text)
+        params = {"event": "accepted", "ascending": "yes"}
+        if begin:
+            params["begin"] = parser.parse(begin).timestamp()
+        if end:
+            params["end"] = parser.parse(end).timestamp()
+        
+        res = mailgun_api("{}/events".format(domain), params)
+        # import pdb
+        # pdb.set_trace()
+        if "items" in res and len(res["items"]) > 0:
+            if params["begin"] > res["items"][0]["timestamp"]:
+                time.sleep(20)
+                res = mailgun_api("{}/events".format(domain), params)
+                if params["begin"] > res["items"][0]["timestamp"]:
+                    context["error"] = "Your request may be blocked! \
+                        Try again after 5 minutes."
+                    return render(request, 'app/events.html', context)
 
-        context['stats'] = json.dumps(response_body)
-        # if "stats" in response_body:
-        # else:
-        #     context['error'] = "no response"
+            for d in res["items"]:
+                hour = datetime.fromtimestamp(d["timestamp"]).hour
+
+                if hour not in new_events:
+                    new_events[hour] = 1
+                else:
+                    new_events[hour] = new_events[hour] + 1
+            
+            events = [(h, new_events[h]) for h in new_events]
+            sorted(events, key=get_second_key)
+            context["new_events"] = events
+        else:
+            context["error"] = "No result!"
+            logger.info("line 146: {}".format(e))                 
+
     except Exception as e:
+        print("line 147: {}".format(str(e)))
         context['error'] = str(e)
-    print(context)
+
     return render(request, 'app/events.html', context)
+
+
+def events_page(request, domain):
+    context = {"domain": domain}
+    if request.method == "POST":
+        begin = request.POST.get("begin", None)
+        end = request.POST.get("end", None)
+        return redirect(reverse("domain-events", args=(domain, begin, end,)))
+    return render(request, 'app/events_page.html', context)
 
 
 
