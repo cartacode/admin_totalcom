9636ce718aa25b2ffcb9d616b5b5d87b40ce2df0
cartacode <cartacode@outlook.com>
2020-01-20 06:49:57 -0500

    Initialize template

---
+++ README.md
@@ -0,0 +1,14 @@
+# Total Comsystem
+
+## How to set up environment
+1. Install dependencies:
+```bash
+pip3 install -r requirements.txt
+```
+
+2. create settings_dev.py under the admin_totalcom folder
+
+## How to run app
+```bash
+python3 manage.py runserver
+```
---
+++ core/views.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import logging
+logger = logging.getLogger(__name__)
+
+from django.shortcuts import render, redirect
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
+from .forms import BaseLoginForm, BaseSignupForm
+from .models import BaseUser
+
+@login_required
+def index(request):
+	return render(request, 'index.html')
+
+# Create your views here.
+def auth_login (request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        context = dict()
+        login_form = BaseLoginForm(request.POST)
+        context['form'] = login_form
+
+        if login_form.is_valid():
+            try:
+                user = BaseUser.objects.get(email=email)
+            except:
+                context['error'] = 'User doesn\'t exists with given email!'
+                return render(request, 'login.html', context)
+
+            user = authenticate(username=email, password=password)
+            context['error'] = 'Password is wrong!'
+
+            print(request.POST)
+            print("@#@#@: ", user)
+            if user is not None:
+                login(request, user)
+                return redirect('/')
+
+        return render(request, 'login.html', context)
+
+    login_form = BaseLoginForm()
+    return render(request, 'login.html', {'form': login_form})
+
+def auth_signup (request):
+    context = dict()
+    if request.method == 'POST':
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm_password')
+
+        signup_form = BaseSignupForm(request.POST)
+        context['form'] = signup_form
+        if signup_form.is_valid():
+            model_instance = signup_form.save(password)
+
+            return redirect('auth_login')
+
+        return render(request, 'signup.html', context)
+
+    signup_form = BaseSignupForm()
+    return render(request, 'signup.html', {'form': signup_form})
